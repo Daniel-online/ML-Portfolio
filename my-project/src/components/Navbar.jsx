a3e8f8c5767523df578d71757a725564c097e1cb
daniel-online <danielplayeraffonso@gmail.com>
2024-10-27 17:59:56 -0300

    Changed Iconsand Navbar

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from "./../assets/images/Logo-Dannyfied_resized.jpg";
+import logo from "./../../public/NeonLogo.png";
 import './Navbar.scss';
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
@@ -7,7 +7,7 @@ import { FaInstagram } from "react-icons/fa";
 const Navbar = () => {
   return <nav className = "navbar flex items-center justify-between py-4">
     <div className = " image flex flex-shrink-0 items-center">
-      <img className=" ml-4 w-10 h-10" src={logo} alt="logo"/>
+      <img className=" ml-4  w-20 h-20" src={logo} alt="logo"/>
     </div>
     <div className="media m-6 flex items-center justify-center gap-4 text-2xl">
         <FaGithub/>
